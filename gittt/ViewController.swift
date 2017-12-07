ae50348c6a726119a3f1e2648bf7930a5456a080
Raffaele Romano <raffaeleromano@10-20-49-31.0200.fed.it.iosda.org>
2017-12-07 17:14:50 +0100

    Initial Commit

---
+++ gittt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gittt
+//
+//  Created by Raffaele Romano on 07/12/17.
+//  Copyright © 2017 Raffaele Romano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
